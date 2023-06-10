addde23120550799ed0e627714c4015b4f420cdc
murray <134729312+murrayart@users.noreply.github.com>
2023-06-10 20:59:33 -0300

    quatrooperacoes.pas
    Adição, Subtração, Multiplicação e Divisão

---
+++ quatrooperacoes.pas
@@ -0,0 +1,20 @@
+program  Contas;
+
+begin 
+
+	writeln("A soma de 2 + 5 é igual a --> ", 2 + 5);
+		writeln("A soma de 15 + 10 é igual a --> ", 15 + 10);
+			writeln("A soma de 100 + 100 é igual a -->", 100 + 100);
+
+writeln("10 - 5 é igual a -- > ", 10 - 5);
+	writeln("200 - 25 é igual a -->", 200 - 25);
+writeln("12841 - 5182 é igual a", 12841 - 5182);
+
+	writeln("2 multiplicado por 10 é igual a -->, " 2 * 10);
+		writeln("5 multiplicado por 15 é igual a ---> ", 5 * 15);
+			writeln("15600 multiplicado por 50 é igual a --> ", 15600 * 50);
+
+writeln("2 dividido por 2 é igual a --> " 2 / 2);
+	writeln("50 dividido por 5 é igual a -->", 50 / 5);
+		writeln(" 256004 dividido por 2 é igual a -->", 256004 / 2);
+end
