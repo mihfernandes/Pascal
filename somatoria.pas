5dce834aa3e8f2b77ac27ee801b841164c18f56f
dartora <134729312+dartorinha@users.noreply.github.com>
2023-06-10 20:33:48 -0300

    somatoria.pas

---
+++ somatoria.pas
@@ -0,0 +1,14 @@
+program Somatoria;
+	num1, num2, soma : integer; 
+
+	x := 15;
+	y := 10;
+	soma := x + y;
+
+begin
+
+	writeln("O valor de num1 é --> ", num1);
+		writeln("O valor de num2 é --> ", num2);
+
+	writeln("A soma desses dois números é de -->", soma);
+end
