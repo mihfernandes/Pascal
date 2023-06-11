6da1f01bd7dc14bd127ff34ed05f03a4a3de0300
murray <134729312+murrayart@users.noreply.github.com>
2023-06-10 21:55:30 -0300

    idade.pas

---
+++ idade.pas
@@ -0,0 +1,20 @@
+program Idade;
+	
+var Idade : integer
+
+	begin
+
+writeln("Insira sua idade: ");
+	read(idade);
+
+	if(idade > 0 and idade < 18);
+
+			writeln("Você é menor de idade!");
+
+		if(idade == 18);
+
+			writeln("Você atingiu a maioridade!"); 
+
+	else 
+         writeln("Você é maior de idade!");
+end
